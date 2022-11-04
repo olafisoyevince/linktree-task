1cfea2b5a62e80b0c4a55ac2594a6139caf9fca9
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2022-11-04 17:58:52 +0100

    style: more styling

--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -1,11 +1,11 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 const ContactMe = () => {
-  // const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(true);
 
-  // const handleChecked = () => {
-  //   setChecked(!checked);
-  // };
+  const handleChecked = () => {
+    setChecked(!checked);
+  };
 
   return (
     <>
@@ -24,7 +24,7 @@ const ContactMe = () => {
               type="text"
               placeholder="Enter your first name"
               id="first_name"
-              className=" h-11 py-2 px-3 border-2 border-gray-300 rounded-lg"
+              className=" h-11 py-2 px-3 border-solid border-2 border-gray-300 rounded-lg focus:border-blue-300 focus:outline-none "
             />
           </div>
 
@@ -34,7 +34,7 @@ const ContactMe = () => {
               type="text"
               placeholder="Enter your last name"
               id="last_name"
-              className=" h-11 py-2 px-3 border-2 border-gray-300 rounded-lg"
+              className=" h-11 py-2 px-3 border-2 border-gray-300 rounded-lg focus:border-blue-300 focus:outline-none"
             />
           </div>
 
@@ -44,7 +44,7 @@ const ContactMe = () => {
               type="email"
               placeholder="youremail@email.com"
               id="email"
-              className=" h-11 py-2 px-3 border-2 border-gray-300 rounded-lg"
+              className=" h-11 py-2 px-3 border-2 border-gray-300 rounded-lg focus:border-blue-300 focus:outline-none"
             />
           </div>
 
@@ -54,14 +54,15 @@ const ContactMe = () => {
               type="text"
               placeholder="Send me a message and I'll reply you as soon as possible"
               id="message"
-              className=" h-32 py-2 px-3 border-2 border-gray-300 rounded-lg"
+              className=" h-32 py-2 px-3 border-2 border-gray-300 rounded-lg focus:border-blue-300 focus:outline-none"
             />
           </div>
 
           <div className=" flex items-start">
             <input
               type="checkbox"
-              className="bg-gray-50 border-2 border-gray-300 focus:ring-3 focus:ring-blue-300 h-7 w-7 rounded-md"
+              className="bg-gray-50 border-2 border-gray-300 focus:ring-3 focus:ring-blue-300 h-7 w-7 rounded-md active:border-2 active:border-blue-200"
+              onChange={() => handleChecked()}
             />
             <p className=" ml-3 text-gray-600 ">
               You agree to providing your data to Seun who may contact you.
@@ -70,7 +71,11 @@ const ContactMe = () => {
 
           <button
             id="btn__submit"
-            className="w-full rounded-lg text-white mt-8 mb-16 bg-blue-600 py-3 px-5"
+            className="w-full rounded-lg text-white mt-8 mb-16 bg-blue-700 py-3 px-5 disabled:border-solid disabled:bg-blue-200  active:bg-blue-600 active:border-blue-200 active:ring"
+            disabled={checked}
+            onClick={(e) => {
+              e.preventDefault();
+            }}
           >
             Send Message
           </button>
