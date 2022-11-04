4bd602012c4ff3c3d88d0206a2c0aec0843821e0
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2022-11-04 15:10:48 +0100

    feat: created the jsx structure for the contact me page

--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -2,9 +2,53 @@ const ContactMe = () => {
   return (
     <>
       <div>
-        <h1>Contact Me</h1>
+        <div>
+          <h1>Contact Me</h1>
+          <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        </div>
 
-        {/* <p></p> */}
+        <form>
+          <div>
+            <label>First Name</label>
+            <input
+              type="text"
+              placeholder="Enter your first name"
+              id="first_name"
+            />
+          </div>
+
+          <div>
+            <label>Last Name</label>
+            <input
+              type="text"
+              placeholder="Enter your last name"
+              id="last_name"
+            />
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input type="email" placeholder="youremail@email.com" id="email" />
+          </div>
+
+          <div>
+            <label>Message</label>
+            <textarea
+              type="text"
+              placeholder="Send me a message and I'll reply you as soon as possible"
+              id="message"
+            />
+          </div>
+
+          <div>
+            <input type="checkbox" />
+            <span>
+              You agree to providing your data to Seun who may contact you.
+            </span>
+          </div>
+
+          <button id="btn__submit">Send Message</button>
+        </form>
       </div>
     </>
   );
